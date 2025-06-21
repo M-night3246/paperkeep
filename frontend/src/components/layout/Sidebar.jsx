df0a41b0d085f7f3625e793ea3905f7186d4ed13
1211103146 <124485196+1211103146@users.noreply.github.com>
2025-06-21 10:36:39 +0800

    Fix header position not fixed while scrolling

--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,40 +1,3 @@
-// import React from 'react';
-// import { FaDollarSign, FaFolder, FaChartLine, FaMapMarkerAlt, FaCalculator, FaDownload } from 'react-icons/fa';
-// import { Link } from 'react-router-dom';
-
-// const Sidebar = ({ isOpen }) => {
-//   return (
-//     <aside className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-//       {/* <div className="sidebar-header">
-//         {isOpen ? <FiX /> : <FiMenu />}
-//         <span className="logo">Paper<span className="highlight">Keep</span></span>
-//       </div> */}
-//       {isOpen && (
-//         <nav className="sidebar-nav">
-//         <SidebarItem icon={<FaDollarSign />} label="Expenses" to="/expenses" />
-//         <SidebarItem icon={<FaFolder />} label="Documents" to="/documents" />
-//         <SidebarItem icon={<FaChartLine />} label="Overview" to="/overview" />
-//         <SidebarItem icon={<FaMapMarkerAlt />} label="Heatmap" to="/heatmap" />
-//         <SidebarItem icon={<FaCalculator />} label="Calculator" to="/calculator" />
-//         <SidebarItem icon={<FaDownload />} label="Export" to="/export" />
-//       </nav>
-//       )}
-//     </aside>
-//   );
-// };
-
-// const SidebarItem = ({ icon, label, active, to }) => (
-//   <Link className={`nav-item ${active ? 'active' : ''}`} to={to}>
-//     <div className="nav-icon">{icon}</div>
-//     <div className="nav-label">{label}</div>
-//   </Link>
-// );
-
-// export default Sidebar;
-
-
-
-
 import React from 'react';
 import { routes } from '../../routes';
 import * as FaIcons from 'react-icons/fa';
@@ -44,10 +7,6 @@ import './layout.css'
 const Sidebar = ({ isOpen }) => {
   return (
     <aside className={`sidebar ${isOpen ? 'open' : 'closed'}`}>
-      {/* <div className="sidebar-header">
-        {isOpen ? <FiX /> : <FiMenu />}
-        <span className="logo">Paper<span className="highlight">Keep</span></span>
-      </div> */}
       {isOpen && (
         <nav className="sidebar-nav">
           {routes
