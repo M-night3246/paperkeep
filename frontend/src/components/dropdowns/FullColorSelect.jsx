a163f3744a108c8df7322bf13477d08d28793f32
1211103146 <124485196+1211103146@users.noreply.github.com>
2025-06-21 10:35:53 +0800

    Update FullColorSelect shape and logic

--- frontend/src/components/dropdowns/FullColorSelect.jsx
+++ frontend/src/components/dropdowns/FullColorSelect.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import './full-color-select.css'
-export default function FullColorSelect({ value, onChange, options, className = "" }) {
+export default function FullColorSelect({ value, onChange, options, className = "", disabled = false, isInt = true, required=true }) {
   const handleChange = (e) => {
-    const selectedId = parseInt(e.target.value);
+    const rawValue = e.target.value;
+    const selectedId = isInt ? parseInt(rawValue) : rawValue;
     const selectedOption = options.find((cat) => cat.id === selectedId) || null;
     onChange(selectedOption);
   };
 
   return (
     <div className={`select-container ${className}`}>
-        <select value={value?.id || ""} onChange={handleChange}>
-        <option value="">Select</option>
+        <select value={value?.id || ""} onChange={handleChange} disabled={disabled} required={required}>
+        <option value="" style={{ fontFamily: "Poppins, sans-serif" }}>Select</option>
         {options.map((cat) => (
             <option key={cat.id} value={cat.id}>
             {cat.name}
