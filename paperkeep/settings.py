d69b6c0b444af534cd0ced608cc5c4614528a8b0
1211103146 <124485196+1211103146@users.noreply.github.com>
2025-06-23 15:20:08 +0800

    Add django_extensions to INSTALLED_APPS

--- paperkeep/settings.py
+++ paperkeep/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "corsheaders",
-    # 'django_extensions',
+    'django_extensions',
     "main",
     "receipts",
     "analytics",
